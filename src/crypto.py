912253d7249d6a29f3120792428c02d33102611d
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-10-01 21:01:49 +0530

    Printing all error messages from within the methods that can go wrong. Saves the pain of telling the client what went wrong everytime something goes wrong.
    Added fetch_public_keys for ease of use.

--- src/crypto.py
+++ src/crypto.py
@@ -4,7 +4,7 @@ from sys import byteorder
 from hashlib import blake2b
 from nacl.pwhash.argon2id import kdf
 from nacl.secret import SecretBox #xsalsa20poly1305
-from nacl.utils import random as nonce_generator
+from nacl.utils import random as nacl_random
 
 
 import src.globals
@@ -26,6 +26,8 @@ def hash(message):
 def insert_public_key(key, keyname):
 	out, err = src.utils.execute("./src/ccr -y -i --name " + keyname, key)
 	if err or out:
+		print("Public key insertion FAILED!")
+		print(out, err)
 		return 1
 	return 0
 
@@ -33,13 +35,26 @@ def insert_public_key(key, keyname):
 def insert_private_key(key, keyname):
 	out, err = src.utils.execute("./src/ccr -y -I --name " + keyname, key)
 	if err or out:
+		print("Private key insertion FAILED!")
+		print(out, err)
 		return 1
 	return 0
+
+
+def fetch_public_keys(keyname):
+	out, err = src.utils.execute("./src/ccr -p -F " + keyname)
+	if err:
+		print("Public key NOT FOUND!")
+		print(out, err)
+		return 1
+	return out
 
 
 def remove_public_key(keyname):
 	out, err = src.utils.execute("./src/ccr -y -x " + keyname)
 	if err or out:
+		print("Public key removal FAILED!")
+		print(out, err)
 		return 1
 	return 0
 
@@ -47,6 +62,8 @@ def remove_public_key(keyname):
 def remove_private_key(keyname):
 	out, err = src.utils.execute("./src/ccr -y -X " + keyname)
 	if err or out:
+		print("Private key removal FAILED!")
+		print(out, err)
 		return 1
 	return 0
 
@@ -56,7 +73,8 @@ def generate_encryption_keys(keyname):
 	print("THESE FILES NEED TO BE (f)LOCKED!!!")
 	out, err = src.utils.execute("./src/ccr --gen-key ENC-256 --name " + keyname)
 	if not bytes("Gathering random seed bits from kernel", 'utf-8') in err:
-		print(err)
+		print("Public key generation FAILED!")
+		print(out, err)
 		return 1
 	return keyname
 
@@ -66,7 +84,8 @@ def generate_signature_keys(keyname):
 	print("THESE FILES NEED TO BE (f)LOCKED!!!")
 	out, err = src.utils.execute("./src/ccr --gen-key SIG-256 --name " + keyname)
 	if not bytes("Gathering random seed bits from kernel", 'utf-8') in err:
-		print(err)
+		print("Signature key generation FAILED!")
+		print(out, err)
 		return 1
 	return keyname
 
@@ -81,7 +100,10 @@ def key_fingerprint(keyname):
 		return 1
 
 	out, err = src.utils.execute("./src/ccr -" + mode + " --fingerprint -F " + keyname)
-	if err: return 1
+	if err:
+		print("Key fingerprinting FAILED!")
+		print(out, err)
+		return 1
 	return bytes.fromhex(''.join(str(out[-81:-2], 'utf-8').split(':')))
 
 
@@ -89,7 +111,10 @@ def asymmetrically_encrypt(message, public_key_name):
 
 	out, err = src.utils.execute("./src/ccr -e -r " + public_key_name, message)
 
-	if not out or err: return 1
+	if not out or err:
+		print("Asymmetric encryption FAILED!")
+		print(out, err)
+		return 1
 	return out
 
 
@@ -97,7 +122,10 @@ def asymmetrically_decrypt(message, private_key_name):
 
 	out, err = src.utils.execute("./src/ccr -d -r " + private_key_name, message)
 
-	if not out or err: return 1
+	if not out or err:
+		print("Asymmetric decryption FAILED!")
+		print(out, err)
+		return 1
 	return out
 
 
@@ -105,7 +133,10 @@ def sign(message, recipient_name):
 
 	out, err = src.utils.execute("./src/ccr -s -r " + recipient_name, message)
 
-	if err: return 1
+	if err:
+		print("Signing FAILED!")
+		print(out, err)
+		return 1
 	return out
 
 
@@ -118,13 +149,20 @@ def verify_signature(signature):
 	# and not a signature verification failure
 	# try forging a signature to see what it returns
 
-	if err or not out: return 1
+	if err or not out:
+		print("Signature Verification FAILED!")
+		print(out, err)
+		return 1
 	return 0
+
+
+def symmetric_key_generator():
+	return nacl_random(SecretBox.KEY_SIZE)
 
 
 def symmetrically_encrypt(message, key):
 	box = SecretBox(key)
-	return box.encrypt(message, nonce_generator(SecretBox.NONCE_SIZE))
+	return box.encrypt(message, nacl_nonce(SecretBox.NONCE_SIZE))
 
 
 def symmetrically_decrypt(message, key):
@@ -132,4 +170,5 @@ def symmetrically_decrypt(message, key):
 	try:
 		return box.decrypt(message)
 	except:
+		print("Symmetric decryption FAILED!")
 		return 1
