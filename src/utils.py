e1e9e32258c4319f405c3b53ef3812634328b4f6
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-10-01 21:24:04 +0530

    Added sizeof, nonce, pack and unpack for ease of use. Added message_id to as a smaller replacement for nonces. Added symmetric_request to add common fields like timestamp and message_id. It also appends the size of the message automatically.

--- src/utils.py
+++ src/utils.py
@@ -2,12 +2,30 @@ from subprocess import Popen, PIPE
 from os import urandom, listdir
 from time import time
 from sh import mkdir, mount, shred, umount, rm, touch
+from msgpack import packb, unpackb
 from sh.contrib import sudo
 from re import search as contains
 from string import printable
 from sys import byteorder
 
+
 import src.globals
+
+
+def sizeof(message):
+	return (len(request)).to_bytes(4, byteorder='little')
+
+
+def nonce(size=src.globals.NONCE_SIZE):
+	return urandom(size)
+
+
+def pack(message):
+	return packb(message, use_bin_type=True)
+
+
+def unpack(message):
+	return unpackb(message, raw=False)
 
 
 def random_name_generator(length=src.globals.RANDOM_NAME_LENGTH):
@@ -18,6 +36,20 @@ def timestamp():
 	# sync server with NTP
 	# unix timestamps are 4 bytes long
 	return round(time()).to_bytes(4, byteorder='little')
+
+
+def message_id():
+	src.globals.MESSAGE_ID += 1
+	return src.globals.MESSAGE_ID
+
+
+def symmetric_request(message):
+
+	message["timestamp"] = timestamp()
+	message["message_id"] = message_id()
+ 	message = src.crypto.symmetrically_encrypt(message, src.globals.KEY)
+
+	return sizeof(message) + message
 
 
 def execute(command, data=None):
@@ -43,14 +75,14 @@ def execute(command, data=None):
 	return (out, err)
 
 
-def mount_ccr_ramdisk(size=1):	# a	cquire file locks!!!
+def mount_ramdisk(size=1):	# a	cquire file locks!!!
 	mkdir('-p', user_home+ccr_folder)
 	print("Mounting temporary filesystem. Need sudo access: ")
 	with sudo:
 		mount("-t", "tmpfs", "-o", "size=" + str(size) + "M", "tmpfs", user_home+ccr_folder)
 
 
-def unmount_ccr_ramdisk():
+def unmount_ramdisk():
 	for _file in listdir(user_home+ccr_folder):
 		if isfile(user_home+ccr_folder+_file):
 			shred("-vfzu", user_home+ccr_folder+_file)
@@ -99,3 +131,22 @@ def password_strength_checker(password):
 		return 1
 	print("Your password's entropy: about", len(password) * 6.5, "bits.")
 	return 0
+
+
+def validate_asymmetric_response(message, nonce):
+	# check for decryption errors
+	plaintext = src.crypto.asymmetrically_decrypt(message, src.globals.SERVER)
+	if type(plaintext) is int:
+		print("Decryption FAILED!")
+		return 1
+
+	plaintext = src.utils.unpack(plaintext)
+	if type(plaintext) is not dict:
+		print("Garbage recived from server!")
+		return 1
+
+	if plaintext["nonce"] != nonce:
+		print("Nonce Verification FAILED!")
+		return 1
+
+	return plaintext
