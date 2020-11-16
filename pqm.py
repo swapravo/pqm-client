33ed6c8ec1d2fa3aca33b2618b81f3e7c2f8dd57
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-11-16 22:42:15 +0530

    Sentry integration...

--- pqm.py
+++ pqm.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+
+import sentry_sdk
 import src.main
 
+"""
+sentry_sdk.init(
+    "https://7103b46a26394672a778debc94e11a81@o468780.ingest.sentry.io/5497332",
+    traces_sample_rate=1.0)
+"""
 
 src.main.main()
