80136b6fe48d7ab0f67cd4fda9177a34da48b987
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-11-16 22:54:04 +0530

    isssqlite3 checks if a proper sqlite3 file or not

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from subprocess import Popen, PIPE
 from os import urandom, listdir
-from os.path import isfile
+from os.path import isfile, getsize
 from time import time
 from sh import mkdir, mount, shred, umount, rm
 from msgpack import packb, unpackb
@@ -8,12 +8,14 @@ from sh.contrib import sudo
 from re import search as contains
 from string import printable
 from sys import byteorder
+from shlex import split, quote
 
 
 import src.globals
 
 
 def sizeof(message):
+	#with 4 bytes you can represent upto 32 GiB
 	return (len(message)).to_bytes(4, byteorder='little')
 
 
@@ -25,8 +27,8 @@ def unpack(message):
 	try:
 		return unpackb(message, raw=False)
 	except:
-		print("Invalid message! Message unpacking FAILED!")
-		return 1
+		print("Invalid message! Unpacking message FAILED!")
+		return None
 
 
 def random_name_generator(length=src.globals.RANDOM_NAME_LENGTH):
@@ -45,33 +47,31 @@ def message_id():
 
 
 def execute(command, data=None):
-    # SANITIZE command
+    # SANITIZE commands here
 
+	process = Popen([command], shell=True, stdout=PIPE, stdin=PIPE)
 	if data:
-		process = Popen([command], shell=True, stdout=PIPE, stdin=PIPE)
+		if type(data) != bytes:
+			print(data)
 		returned_data = process.communicate(input=data)
 	else:
-		process = Popen([command], shell=True, stdout=PIPE, stderr=PIPE)
 		returned_data = process.communicate()
-
 	process.terminate()
-	out = err = 0
-	if returned_data[0] == b'' or returned_data[0] is None:
-		out = 0
-	else:
+
+	out, err = None, None
+	if returned_data[0]:
 		out = returned_data[0]
-	if returned_data[1] == b'' or returned_data[1] is None:
-		err = 0
-	else:
+	if returned_data[1]:
 		err = returned_data[1]
 	return (out, err)
 
 
-def mount_ramdisk(size=1):	# a	cquire file locks!!!
+def mount_ramdisk(size=1):	# acquire file locks!!!
 	mkdir('-p', src.globals.USER_HOME+src.globals.CCR_FOLDER)
 	print("Mounting temporary filesystem. Need sudo access: ")
 	with sudo:
-		mount("-t", "tmpfs", "-o", "size=" + str(size) + "M", "tmpfs", src.globals.USER_HOME+src.globals.CCR_FOLDER)
+		mount("-t", "tmpfs", "-o", "size=" + str(size) + "M", "tmpfs", \
+			src.globals.USER_HOME+src.globals.CCR_FOLDER)
 
 
 def unmount_ramdisk():
@@ -84,44 +84,57 @@ def unmount_ramdisk():
 	rm("-rfv", src.globals.USER_HOME+src.globals.CCR_FOLDER)
 
 
-def username_vailidity_checker(username):
-
-	# USE THE STANDARD
+def username_is_vailid(username):
 
 	# check for curse words, commands, illegal symbols
 	if len(username) < 3 or len(username) > 128:
 		print("Username must be atleast four and less than 129 charcters.")
-		return 1
+		return False
 	username = username.lower()
 	allowed_characters = printable[:36] + '_.'
-	cleaned_username = ''.join(list(filter(lambda x: x in allowed_characters, username)))
+	cleaned_username = ''.join(list(filter(lambda x: x in allowed_characters, \
+		username)))
 
 	if username != cleaned_username:
-		print("Illegal characters present. Allowed charcters: ", ' '.join(list(allowed_characters)))
-		return 1
-	return 0
+		print("Illegal characters present. Allowed charcters: ", \
+			' '.join(list(allowed_characters)))
+		return False
+	return True
 
 
-def password_strength_checker(password):
+def password_is_strong(password):
 
-	print("REMOVE THIS RETURN STATEMENT. FOR DEVELOPMENT PURPOSES ONLY!!!")
-	return 0
+	print("Bypassing password strength check!")
+	return True
 	# alteast 128 bits of entropy required => 20 characters atleast
 	# log2(26 + 26 + 10 + 33)
 	if len(password) < 20:
 		print("Not enough Entropy. Password needs to be longer!")
-		return 1
+		return False
 	if contains("[a-z]", password) is None:
 		print("Small letters needed!")
-		return 1
+		return False
 	if contains("[A-Z]", password) is None:
 		print("Capital letters needed!")
-		return 1
+		return False
 	if contains("[0-9]", password) is None:
 		print("Numbers needed!")
-		return 1
+		return False
 	if contains("[" + printable[62:-5] + "]", password) is None:
 		print("Special characters needed!")
-		return 1
+		return False
 	print("Your password's entropy: about", len(password) * 6.5, "bits.")
-	return 0
+	return True
+
+
+def issqlite3(filename):
+
+    if not isfile(filename):
+        return False
+    if getsize(filename) < 100:
+		# SQLite database file header is 100 bytes
+        return False
+    with open(filename, 'rb') as fo:
+        header = fo.read(100)
+
+    return header[:16] == b'SQLite format 3\x00'
