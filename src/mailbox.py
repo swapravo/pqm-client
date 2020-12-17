53004b185807bbbcc85771b44f3027b8981151ae
Swapravo Sinha Roy <swapravo@tutanota.com>
2020-12-17 18:19:54 +0530

    Streamlined the shape of the (temporary) mail object. Made an "envelope" which basically has signatures and addresses.
    Wrote the sections that encrypt and decrypt mails.
    
    print_mailbox should be displaying all the different folders in the 
    user's mailbox.
    
    process_mail recieves a raw mail from the server, decrypts and validates 
    it before passing it onto a db function (that should be inserting it 
    into the user's database)

--- src/mailbox.py
+++ src/mailbox.py
@@ -29,22 +29,22 @@ def process(message, response_size)
         return (out, 1)
     del request
 
-    response = src.crypto.symmetrically_decrypt(response)
-    if type(response) is int:
-        return (out, 1)
+    response, err = src.crypto.symmetrically_decrypt(response)
+    if err:
+        return (None, 1)
 
     response = src.utils.unpack(response)
     if type(response) is not dict:
-        return (out, 1)
+        return (None, 1)
 
     if response["message_id"] != message_id:
         print("message ID MISMATCH!")
-        return (out, 1)
+        return (None, 1)
 
     # do look into rehashing speeds
     del response["message_id"]
     err = 0
-    return response, 0
+    return (response, 0)
 
 
 def download_public_keys(*IDs):
@@ -70,14 +70,14 @@ def download_public_keys(*IDs):
     else:
         return(out, err)
 
-    response, err = process(
+    response, err = src.mailbox.process(
         {"request_code": src.globals.GET_PUBLIC_KEYS, "mail_id": valid_IDs}, \
             src.globals.BIG_RESPONSE)
 
     if err:
         return (None, 1)
 
-    # process the response and insert the public keys
+    # process the response and INSERT THE PUBLIC KEYS
     # validate these keys before inserting them into
     # the keyring. Display a QR code-like graphical
     # representation also.
@@ -96,6 +96,8 @@ def download_public_keys(*IDs):
     """
 
     #VALIDATE RESPONSE HERE
+    # print it out if the public key corresponding
+    # to an username is not found!
 
     for username, keys in response.items():
         if src.crypto.key_is_valid(keys["encryption_public_key"]) and
@@ -117,16 +119,24 @@ def download_public_keys(*IDs):
 
 
 def refresh_mailbox():
+    # this only downlads the metadata about the emails
+    # if you want a certain mail, you download_mail() with the idea
+    # of the emails
 
-	response = process(
+	out, err = src.mailbox.process(
         {"request_code": src.globals.UPDATE_MAILBOX}, src.globals.BIG_RESPONSE)
 
-    if type(response) is int:
-        print("FAILED to refresh mailbox!")
-        return 1
+    if err:
+        print("Failed to refresh mailbox! Error:", err)
+        return (None, 1)
 
-    # sync mails with offline db here
-    return 0
+    if out == src.globals.NO_CHANGES_IN_MAILBOX:
+        print("No changes in mailbox!")
+        return (None, 0)
+    else:
+        # parse email metadata here and insert it into the db
+        # update the current view?
+        pass
 
 
 def delete_mail(from, *uid):
@@ -141,16 +151,22 @@ def delete_mail(from, *uid):
 
 	# DELETE MAIL FROM THE OFFLINE DB
 	# OR DELETE MAIL FROM OFFLINE DB AND THEN SYNC THE DB
+    # DECIDE ON HOW THE EMAILS WILL BE INDEXED AND UNIQUELY IDENTIFIED
+
     if from:
-    	response = process(
+    	response, err = process(
             {"request_code": src.globals.DELETE_EMAIL, "email_uid": uid}, src.globals.SMALL_RESPONSE)
 
-        if type(response) is int:
+        if err:
             print("FAILED to delete mail!")
-            return 1
+            return (None, 1)
 
     if from == 0 or from == 2:
         # delete stuff from the offline db
+        for each_uid in uid:
+            out, err = src.db.delete_mail(each_uid)
+            if err:
+                print("Failed to delete mail", each_uid, "!\n\tError:", err)
 
 
 def send_mail(to, cc, bcc, subject, body, *attachments):
@@ -159,119 +175,151 @@ def send_mail(to, cc, bcc, subject, body, *attachments):
     cc, bcc are lists of email addresses.
     subject, body are simple utf-8 strings.
     attachments must be file paths and NOT file objects.
+
+    if we have fields for addresses "inside" the email, the
+    server wont be able to read them...
+    if we send a copy of the addresses along with the email,
+    someone will tweak the source
+    to fuck with the list of recipients.
+    Breaking up the email into multiple parts: addresses, text content, attachments
+    will unnecessariliy complicate stuff.
+    What the server can do is ask the user to sign the
+    addresses that it says it wants to send the mails to,
+    in addition to the emails, which already have all the necessary fields:
+    addresses, text, attachments.
     """
 
-    mail =  { \
-
-        "text": { \
-
-            "content": { \
-
-                "addresses": { \
-                    "to": None, \
-                    "cc": None, \
-                    "bcc": None }, \
-
-                "key": None,
-
-                "text": { \
-                    "subject": None, \
-                    "body": None } \
-                        }, \
-
-            "signature": None}, \
-
-        "attachments": { \
-
-            "content" : {}, \
-
-            "signature": None
-                        } \
-            }
+    if len(cc) + len(bcc) + 1 > src.globals.MAX_RECIPIENTS:
+        print("The maximum number of recipients you can add to an email is ", \
+        src.globals.MAX_RECIPIENTS)
+        return (None, 1)
 
     if attachments:
         size = 0
-        attached = {}
+        media = {}
         for attachment in attachments:
             if isfile(attachment):
                 size += getsize(attachment)
                     if size > src.globals.MAX_MAIL_SIZE:
                         print("Maximum mail size limits exceeded!")
-                        return 1
+                        return (None, 1)
                 fo = open(attachment, "rb")
                 # split gives me the filename
-                attached[split(attachment)[1]] = fo.read()
+                media[split(attachment)[1]] = fo.read()
                 fo.close()
             else:
                 print(attachment, "NOT FOUND!")
-                return 1
-        attached = src.utils.pack(attached)
-    else:
-        del message["attachments"]
+                return (None, 1)
 
-    # ASSERT SUBJECT LENGTH LIMITS (ONLY) WHEN SENDING MAILS
+    # ASSERT SUBJECT LENGTH LIMITS (? / ONLY) WHEN SENDING MAILS
     # TO NON-DOMESTIC SERVERS BECAUSE OTHERWISE THEY'LL
     # GET TRUNCATED!
 
-    text_content = { \
-        "subject": subject, \
-        "body": body}
-    text_content = src.utils.pack(text_content)
+    recipients = [to] + cc + bcc
+    new_keys = []
 
+    for recipient in recipients:
+        if not src.crypto.key_exists_in_keyring(recipient):
+            new_keys.append(recipient)
 
-    if len(cc) + len(bcc) + 1 > src.globals.MAX_RECIPIENTS:
-        print("The maximum number of recipients you can add to an email is ", src.globals.MAX_RECIPIENTS)
+    if new_keys:
+        out, err = src.mailbox.download_public_keys(new_keys)
+        if err:
+            return (None, 1)
 
-    old_keys, new_keys = [], []
-    if src.crypto.key_missing(to):
-        new_keys.append(to)
-    else:
-        old_keys.append(to)
-    for address in cc:
-        if src.crypto.key_missing(address):
-            new_keys.append(address)
-        else:
-            old_keys.append(address)
-    for address in bcc:
-        if src.crypto.key_missing(address):
-            new_keys.append(address)
-        else:
-            old_keys.append(address)
+    # tell the server that we'll be sending quite a lot of data!
+    # request = {"request_code": src.globals.SEND_MAIL}
+    # please fix the cc bcc issue. i am really sick of it
 
-    keys_found, keys_not_found = download_public_keys(new_keys)
-    old_keys.append(keys_found)
+    mail = src.utils.pack({
+        "timestamp": src.utils.timestamp(),
+        "text": {
+            "to": to,
+            "cc": cc,
+            "bcc": bcc,
+            "subject": subject,
+            "body": body,
+        "attachments": media
+            }})
 
-    if keys_not_found:
-        print("The following keys were NOT FOUND: ")
-        for i in keys_not_found:
-            print(i)
+    envelope = {
+        "signed_mail": {
+            "signature": None,
+            "mail": None,
+            "key": None},
+        "signed_addresses": src.crypto.sign(src.utils.pack({
+            "to": to,
+            "cc": cc,
+            "bcc": bcc}), src.globals.SERVER),
+        "sender": None}
+        # the server fills the sender field
 
-    if to not in old_keys:
-        print("Reciepient Address NOT FOUND!")
-        return 1
+    # i am assuming that i am working with a mail that just needs to be
+    # encrypted and sent over to the sever
+    # hash of the mail will be calculated on the plaintext
 
-    request = {"request_code": src.globals.SEND_MAIL}
+    mail_hash = src.crypto.hash(mail)
 
-    # work with only those keys that have been found
-    cc = list(set(cc) & set(old_keys))
-    bcc = list(set(bcc) & set(old_keys))
+    for recipient in recipients:
+        if src.crypto.key_exists_in_keyring(recipient):
+            key = src.crypto.symmetric_key_generator()
+            encrypted_mail = src.crypto.symmetrically_encrypt(mail, key)
+            signature = src.crypto.sign(mail_hash, recipient)
+            # asymmetrically_encrypt the key now and add it to the message
+            key = src.crypto.asymmetrically_encrypt( \
+                key, src.crypto.encryption_key(recipient))
+            envelope["signed_mail"]["signature"] = signature
+            envelope["signed_mail"]["mail"] = encrypted_mail
+            envelope["signed_mail"]["key"] = key
 
-    """
-        key = src.crypto.symmetric_key_generator()
-
-        encrypted_text_content = src.crypto.symmetrically_encrypt(text_content, key)
-        text_content_signature = src.crypto.sign(src.crypto.hash(encrypted_text_content), )
-
-        if attachments:
-            encrypted_attachments = src.crypto.symmetrically_encrypt(attached, key)
-            attachment_signature = src.crypto.sign(src.crypto.hash(encrypted_attachments, )
-    """
-
+        src.network.process({'request_code': src.globals.SEND_MAIL, \
+            'request': envelope}, src.globals.SMALL_RESPONSE)
 
 def write_mail():
 
-    to = input("To: ")
-    cc = input("CC: ")
-    bcc = input("BCC: ")
+    to = src.utils.writer().split()
+    cc = src.utils.writer().split()
+    bcc = src.utils.writer().split()
+
+    content = src.utils.writer()
+
+    # do something that helps the user to attach media
+    # parse the whole thing into a "mail object"
 
 	return
+
+
+def print_mailbox():
+    # print the different FOLDERS in inbox
+    pass
+
+
+def process_mail(signed_mail):
+
+    """
+    emails downladed straight from the server (the envelope) need to
+    be processed before they're inserted into the user's database
+    """
+
+    # perform data validations both on the server and the recipient's device
+
+    key = src.crypto.asymmetrically_decrypt(signed_mail["key"], \
+        src.crypto.encryption_key(src.globals.USERNAME))
+    if key is None:
+        return (None, 1)
+
+    mail = src.crypto.symmetrically_decrypt(signed_mail["mail"], key)
+    if mail is None:
+        return (None, 1)
+
+    hash, err = src.crypto.verify_signature(signed_mail["signature"])
+    if err:
+        return (None, 1)
+
+    if hash != src.crypto.hash(mail):
+        print("Hash verification of email FAILED!")
+        return (None, 1)
+
+    # now that the mail has been processed, insert it into the db
+
+    return (None, 0)
